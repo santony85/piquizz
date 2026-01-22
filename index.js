392d98b78785222e94c3df0ee7f6f8c5fd0bec33
santony85 <antony_santero@hotmail.com>
2026-01-22 09:55:57 +0100

    Close to end

--- index.js
+++ index.js
@@ -1,129 +1,159 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const wss = new WebSocket.Server({ server });
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
+// EJS
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-const fs =require("fs"); 
-const questions = JSON.parse( fs.readFileSync("question.json", "utf8") );
-
+// Questions
+const questions = JSON.parse(fs.readFileSync("question.json", "utf8"));
 let question = {};
-
-// use res.render to load up an ejs view file
-
 let players = [
-    "Joueur 1",
-    "Joueur 2",
-    "Joueur 3",
-    "Joueur 4",
-    "Joueur 5"       
-]
-
-let userquestion = [
-    { "categorie": "geographie", "question": "Quelle est la capitale de l'Espagne ?", "reponse": "Madrid" },
-    { "categorie": "geographie", "question": "Quel pays possède la plus longue côte maritime ?", "reponse": "Canada" },
-    { "categorie": "geographie", "question": "Quel est le plus grand lac d'Afrique ?", "reponse": "Lac Victoria" }
+  "Joueur 1",
+  "Joueur 2",
+  "Joueur 3",
+  "Joueur 4",
+  "Joueur 5"
 ];
 
+let userquestion = [
+  { categorie: "geographie", question: "Quelle est la capitale de l'Espagne ?", reponse: "Madrid" },
+  { categorie: "geographie", question: "Quel pays possède la plus longue côte maritime ?", reponse: "Canada" },
+  { categorie: "geographie", question: "Quel est le plus grand lac d'Afrique ?", reponse: "Lac Victoria" }
+];
 
-  
+let isFirst = false;
+let isStart = false;
+
 function getRandomQuestion() {
-    const index = Math.floor(Math.random() * questions.length);
-    return questions[index];
+  const index = Math.floor(Math.random() * questions.length);
+  return questions[index];
 }
 
-
-// index page
-app.get('/', function(req, res) {
-    question = getRandomQuestion();
-    console.log(question);
-    res.render('index',{players : players,question:question}); 
-    io.emit("question", question); 
+// --- Pages ---
+app.get("/", (req, res) => {
+  question = getRandomQuestion();
+  isFirst = false;
+  console.log("ici");
+  res.render("index", { players, question });
+  // 🔥 Envoi propre aux ESP32 et navigateurs 
+  broadcast(`QUESTION:${JSON.stringify(question)}`); 
+  broadcast("RESTART");  
 });
 
-// index page
-app.get('/newpart', function(req, res) {
-    res.render('newpart');  
+app.get("/anim", (req, res) => {
+  res.render("anim", { players, question, userq: userquestion });
 });
 
-app.post('/addpart', function(req, res) {
-    console.log(req.body)
-    players[0]= req.body.j1;
-    players[1]= req.body.j2;
-    players[2]= req.body.j3;
-    players[3]= req.body.j4;
-    players[4]= req.body.j5;
-    io.emit("players", players);
-    res.render('anim',{players : players});
+app.get("/newpart", (req, res) => {
+  res.render("newpart");
 });
 
-app.get('/newquestion', function(req, res) {
-    res.render('newquestion');
+app.post("/addpart", (req, res) => {
+  players[0] = req.body.j1;
+  players[1] = req.body.j2;
+  players[2] = req.body.j3;
+  players[3] = req.body.j4;
+  players[4] = req.body.j5;
+  broadcast(`PLAYERS:${JSON.stringify(players)}`);
+  res.render("anim", { players, question, userq: userquestion });
 });
 
-app.post('/addquestion', function(req, res) {
-    console.log(req.body)
-    userquestion.push(req.body);
-    io.emit("userquestion", userquestion);
-    res.render('newquestion');
+app.get("/newquestion", (req, res) => {
+  res.render("newquestion");
 });
 
-app.get('/anim', function(req, res) {
-    console.log(userquestion);
-    res.render('anim',{players : players,question:question,userq:userquestion});
+app.post("/addquestion", (req, res) => {
+  userquestion.push(req.body);
+  broadcast(`USERQ:${JSON.stringify(userquestion)}`);
+  res.render("newquestion");
 });
 
-io.on('connection', (socket) => {
-    console.log("ESP32 connecté :", socket.id);
+// --- WebSocket helpers ---
+function broadcast(msg) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg);
+    }
+  });
+}
 
-    socket.on("message", (data) => {
-        console.log("Message reçu de l'ESP32 :", data);
-        //console.log("Message reçu de l'ESP32 :", players[data-1]);
-        io.emit("winner", data);
-    });   
+// --- WebSocket server ---
+wss.on("connection", (ws, req) => {
     
-    socket.on("anim", (data) => {      
-        console.log("Message reçu de anim:", data);  
-        //io.emit("winner", { player: data });
-        if(data==="next"){
-            console.log("next")
-            question = getRandomQuestion();
-            io.emit("question", question);   
-        }        
-        else if(data==="continue"){
-            console.log("continue");
-            io.emit("restartTimer");
-        }
-    });
-    
-    socket.on("userq", (data) => {
-        console.log("Message reçu anim", data);
-        console.log(userquestion[data]);
-        io.emit("question", userquestion[data]); 
-        userquestion.splice(data, 1);
-        io.emit("userquestion", userquestion);
-        //del question
-        //io.emit("timer", data );
-    });
-    
-    socket.on("timer", (data) => {
-        console.log("Message reçu de timer:", data);
-        io.emit("timer", data );
-    });
-    
-    // Exemple : envoyer un message à l’ESP32
-    socket.emit("message", "ON");
+  const ip = req.socket.remoteAddress;
+  console.log("Client WS connecté :", ip);
+
+  // On peut envoyer l'état initial
+  ws.send(`QUESTION:${JSON.stringify(question)}`);
+  ws.send(`PLAYERS:${JSON.stringify(players)}`);
+  ws.send(`USERQ:${JSON.stringify(userquestion)}`);
+
+  ws.on("message", (message) => {
+    const msg = message.toString();
+    console.log("WS message reçu :", msg);
+
+    // BUZZ:2
+    if (msg.startsWith("BUZZ:")) {
+      const joueur = msg.split(":")[1];
+
+      if (!isFirst && isStart) {
+        isFirst = true;
+        isStart = false;
+        console.log("Winner :", joueur);
+        broadcast(`WINNER:${joueur}`);
+      }
+      return;
+    }
+
+    // ANIM:NEXT
+    if (msg === "ANIM:NEXT") {
+      isFirst = false;
+      question = getRandomQuestion();
+      broadcast(`QUESTION:${JSON.stringify(question)}`);
+      broadcast("RESTART");
+      return;
+    }
+
+    // ANIM:CONTINUE   
+    if (msg === "ANIM:CONTINUE") {
+      isFirst = false;
+      isStart = true;
+      broadcast("RESTART_TIMER");
+      return;
+    }
+
+    // USERQ:INDEX
+    if (msg.startsWith("USERQ:")) {
+      const index = parseInt(msg.split(":")[1], 10);
+      if (!isNaN(index) && userquestion[index]) {
+        const q = userquestion[index];
+        broadcast(`QUESTION:${JSON.stringify(q)}`);
+        userquestion.splice(index, 1);
+        broadcast(`USERQ:${JSON.stringify(userquestion)}`);
+      }
+      return;
+    }
+
+    // TIMER:xxx
+    if (msg.startsWith("TIMER:")) {
+      broadcast(msg); // on rebroadcast tel quel
+      return;
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client WS déconnecté :", ip);
+  });
 });
 
 server.listen(3000, () => {
-    console.log("Serveur Socket.IO v2 sur http://localhost:3000");
+  console.log("Serveur WebSocket Piquizz sur http://localhost:3000");
 });
